be143d4fdbae2fa6beb3c774abd70f0a63da61d6
Jose Rene Familia <familiajoserene@gmail.com>
2023-09-26 17:01:44 -0400

    Alurageek v.1.0 Added

---
+++ controllers/actualiza-producto-controller.js
@@ -0,0 +1,28 @@
+const inputImageUrl = document.querySelector("[data-url]");
+const inputNombre = document.querySelector("[data-nombre]");
+const inputPrecio = document.querySelector("[data-precio]");
+const inputDescripcion = document.querySelector("[data-descripcion]");
+
+productoServices.listarUnProducto(id).then((datos) => {
+  inputImageUrl.setAttribute("src", datos.imageUrl);
+  inputNombre.value = datos.name;
+  inputPrecio.value = datos.price;
+  inputDescripcion.value = datos.description;
+});
+
+const formulario = document.querySelector("[data-form]");
+
+formulario.addEventListener("submit", (evento) => {
+  evento.preventDefault();
+
+  productoServices
+    .alteraProducto(
+      id,
+      inputNombre.value,
+      inputPrecio.value,
+      inputDescripcion.value
+    )
+    .then(() => {
+      window.location.href = "../screens/index.html";
+    });
+});
